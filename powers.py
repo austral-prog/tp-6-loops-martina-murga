27907c802e37d636078695feb80a99de6ab47f7b
martina-murga <mmurga@mail.austral.edu.ar>
2026-05-05 09:50:10 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -7,8 +7,10 @@ def power(base, exp):
 
     Ejemplo: power(2, 3) -> 8  (2*2*2)
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    result = 1
+    for i in range(exp):
+        result = result * base
+    return result
 
 def sum_of_powers(base, max_exp):
     """
@@ -17,4 +19,7 @@ def sum_of_powers(base, max_exp):
 
     Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    total = 0
+    for i in range(max_exp + 1):
+        total = total + (base ** i)
+    return total
